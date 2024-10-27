4f978a84aef35b7a38980f3bd464b1d248a77da9
rp0x1 <skids2themoon@proton.me>
2024-10-28 00:57:20 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
- console.log("Hello, world.")
+#!/usr/bin/env node
+
+const typeMeat = process.argv[2] || undefined;
+const quantityOfMeat = process.argv[3] || undefined;
+
+const newMeat = {
+    typeOfMeat: typeMeat,
+    quantityInGrams: quantityOfMeat
+}
+
+console.log(newMeat);
