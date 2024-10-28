9cc7751e0911c1287a38e88464a342cceb4795b9
rp0x1 <skids2themoon@proton.me>
2024-10-28 15:20:04 +0100

    Added utils folder & cleaned-up the working tree

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
+//const utils = require("./utils/index");
 
 const typeMeat = process.argv[2] || undefined;
 const quantityOfMeat = process.argv[3] || undefined;
 
 const newMeat = {
-    typeOfMeat: typeMeat,
-    quantityInGrams: quantityOfMeat
+    typeMeat,
+    quantityOfMeat
 }
 
 console.log(newMeat);
